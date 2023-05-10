4fd83e5ce35772b0f8525e9d932803f660746ae9
YeonSeoJo <irwinjoebeck@gmail.com>
2023-05-10 23:57:00 +0900

    first function

--- class/01-01-token/index.js
+++ class/01-01-token/index.js
@@ -1,1 +1,11 @@
 console.log('hello World!');
+
+function getToken(aaaa) {
+  const result = String(Math.floor(Math.random() * 10 ** aaaa)).padStart(
+    aaaa,
+    '0'
+  );
+  console.log(result);
+}
+
+getToken(3);
