b2128866649de594316287c2b8841414a27758dd
YeonSeoJo <irwinjoebeck@gmail.com>
2023-05-16 20:26:37 +0900

    token-count2

--- 01-02-token-count/index.js
+++ 01-02-token-count/index.js
@@ -4,6 +4,9 @@ function getToken(aaaa) {
   if (aaaa === undefined) {
     console.log('error, 갯수를 제대로 입력하세요');
     return;
+  } else if (aaaa <= 0) {
+    console.log('error, 갯수가 너무 적습니다.');
+    return;
   }
 
   const result = String(Math.floor(Math.random() * 10 ** aaaa)).padStart(
