36954edf1f604a6716be34f55eb9c58ce071260f
YeonSeoJo <irwinjoebeck@gmail.com>
2023-05-19 23:54:25 +0900

    dir path fix

---
+++ class/01-02-token-count/index.js
@@ -0,0 +1,22 @@
+console.log('hello World!');
+
+function getToken(aaaa) {
+  if (aaaa === undefined) {
+    console.log('error, 갯수를 제대로 입력하세요');
+    return;
+  } else if (aaaa <= 0) {
+    console.log('error, 갯수가 너무 적습니다.');
+    return;
+  } else if (aaaa > 10) {
+    console.log('error, 갯수가 너무 많습니다.');
+    return;
+  }
+
+  const result = String(Math.floor(Math.random() * 10 ** aaaa)).padStart(
+    aaaa,
+    '0'
+  );
+  console.log(result);
+}
+
+getToken(3);
